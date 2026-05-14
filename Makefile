e8f008b8bc90f8d4f8c8e041825922a98c36d89b
Azamat <aza.saitbot@yandex.ru>
2026-05-14 10:08:18 +0300

    set env time zone and cleanup log

--- Makefile
+++ Makefile
@@ -71,7 +71,14 @@ env-port-forward:
 
 env-port-close:
 	@docker compose down port-forwarder 
-
+log-cleanup:
+	@read -p "Очистить все log файлы? Опасно утеря логов? [y/N] " ans; \
+	if [ "$$ans" = "y" ]; then \
+		sudo rm -rf ${PROJECT_ROOT}/out/logs && \
+		echo "Файлы логов очищены"; \
+	else \
+		echo "Очистка логов отменена"; \
+	fi
 todoapp-run:
 	@export LOGGER_FOLDER=$(PROJECT_ROOT)/out/logs && \
 	export POSTGRES_HOST=localhost && \
--- cmd/todoapp/main.go
+++ cmd/todoapp/main.go
@@ -8,6 +8,7 @@ import (
 	"syscall"
 	"time"
 
+	core_config "github.com/saitbatalov-go/golang-todoapp/internal/core/config"
 	core_logger "github.com/saitbatalov-go/golang-todoapp/internal/core/logger"
 	core_pgx_pool "github.com/saitbatalov-go/golang-todoapp/internal/core/repository/postgres/pool/pgx"
 	core_http_middleware "github.com/saitbatalov-go/golang-todoapp/internal/core/transport/http/middleware"
@@ -21,12 +22,10 @@ import (
 	"go.uber.org/zap"
 )
 
-var (
-	timeZone = time.UTC
-)
-
 func main() {
-	time.Local = timeZone
+
+	cfg := core_config.NewConfigMust()
+	time.Local = cfg.TimeZone
 
 	ctx, cancel := signal.NotifyContext(
 		context.Background(),
@@ -51,7 +50,7 @@ func main() {
 	}
 	defer pool.Close()
 
-	logger.Debug("application time zone", zap.Any("zone", timeZone))
+	logger.Debug("application time zone", zap.Any("zone", time.Local))
 
 	logger.Debug("initializing feature", zap.String("feature", "users"))
 	usersRepository := user_postgres_repository.NewUsersRepository(pool)
