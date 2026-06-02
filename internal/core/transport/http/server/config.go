ce3417d1853c0980f5cbe6a6f656fe4523dca6c0
Azamat <aza.saitbot@yandex.ru>
2026-06-03 00:30:24 +0300

    фыв

--- internal/core/transport/http/server/config.go
+++ internal/core/transport/http/server/config.go
@@ -10,7 +10,7 @@ import (
 type Config struct {
 	Addr            string        `envconfig:"ADDR" required:"true"`
 	ShutdownTimeout time.Duration `envconfig:"SHUTDOWN_TIMEOUT" default:"30s"`
-	AllowedOrigins  []string      `envconfig:"ALLOWED_ORIGINS" required:"true"`
+	AllowedOrigins  []string      `envconfig:"HTTP_ALLOWED_ORIGINS" required:"true"`
 }
 
 func NewConfig() (Config, error) {
